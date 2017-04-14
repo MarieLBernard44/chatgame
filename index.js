48f91ea5a6181e6cf21781d45de2735441f1bcde
MarieLBernard44 <marielouise.bernard44@gmail.com>
2017-04-14 12:36:05 +0200

    Add /nick command

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const uuid = require('uuid/v4');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-//const settings = require('settings');
+const settings = require('./settings');
 
-const port = 8080;
 
 // Instanciation du serveur web, de Express et de Socket.io
 const app = express();
@@ -12,8 +11,8 @@ const server = http.Server(app);
 const io = socketio(server);
 
 // Lancement du serveur HTTP
-server.listen(port, () => {
-    console.log('Listening on port ' + port);
+server.listen(settings.port, () => {
+    console.log('Listening on port ' + settings.port);
 });
 
 // Contenu du dossier public accessible sur le web
@@ -21,21 +20,27 @@ app.use('/', express.static(__dirname + '/public'));
 
 // Liste des connectés
 const users = [];
-const msgs = [];
 
 // Connexion des clients socket.io
 io.on('connection', (socket) => {
     console.log('User (' + socket.id + ') vient de se connecter');
 
-    // Ajout d'un connecté
+        // Ajout d'un connecté
     const user = {
         id: socket.id,
-        nickname: socket.id
+        // Nom au hasard dans la liste des noms par défaut
+        nickname: settings.defaultNicknames[Math.floor(Math.random() * settings.defaultNicknames.length)],
+        // Position initiale du user à l'écran
+        position: {
+            x: Math.random() * 100,
+            y: Math.random() * 100
+        }
     };
     users.push(user);
 
     // Diffusion de la liste de connectés à tout le monde
     io.emit('users', users);
+
 
     // Déconnexion de l'utilisateur
     socket.on('disconnect', () => {
@@ -46,24 +51,23 @@ io.on('connection', (socket) => {
         io.emit('users', users);
     });
 
-
-
-
     // Réception d'un nouveau message
     socket.on('msg', (txt) => {
 
         // Ajout d'un message
-        const msg = {
-            id : uuid,
+        const message = {
+            id : uuid(),
             userId: user.id,
             txt: txt,
-            date: new Date().getTime()
+            date: new Date().getTime(),
         };
-
-
         // Diffusion du message auprès de tous les connectés
-        io.emit('msg', msg);
+        io.emit('msg', message);
 
+        socket.on('nick',(nickname) => {
+            user.nickname = nickname;
+            io.emit('users', users);
+        });
 
     });
 
